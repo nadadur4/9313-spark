478e6a6a4e892258cce262b5fcca3486a45e71f3
NadServer <nadserver@nadserver.local>
2025-07-21 15:03:19 +1000

    Lab5 Done; conda envs

--- labs/05/5-02.py
+++ labs/05/5-02.py
@@ -1,8 +1,9 @@
 from pyspark import SparkContext, SparkConf
+from operator import add
 import sys
 
 class Answer:
-    def run(self, inputPath, outputPath)
+    def run(self, inputPath, outputPath):
         conf = SparkConf().setAppName("problem2")
         sc = SparkContext(conf=conf)
         
@@ -22,40 +23,44 @@ class Answer:
                       .count()
         )
 
-        print(distinctItemTypes.collect())
+        with open("/home/nadserver/spark-home/labs/05/Q3.txt", "w") as Q3:
+            print(distinctItemTypes, file=Q3)
 
         # Question 4
         bidsPerItemType = (
             auctionRDD.map(lambda line: (line[itemtype], 1))
-                      .reduceByKey()
+                      .reduceByKey(add)
         )
 
-        print(bidsPerItemType.collect())
+        with open("/home/nadserver/spark-home/labs/05/Q4.txt", "w") as Q4:
+            print(bidsPerItemType.collect(), file=Q4)
 
         # Question 5
         maximumBidAuction = (
             auctionRDD.map(lambda line: (line[aucid], 1))
-                      .reduceByKey()
-                      .reduce(lambda a, b: max(a[1], b[1]))
+                      .reduceByKey(add)
+                      .reduce(max)
         )
 
-        print(maximumBidAuction.collect())
+        with open("/home/nadserver/spark-home/labs/05/Q5.txt", "w") as Q5:
+            print(maximumBidAuction, file=Q5)
 
         # Question 6
         top5Items = (
-            auctionRDD.map(lambda line: (line, 1))
-                      .reduceByKey()
-                      .sortBy(lambda line: line[1])
+            auctionRDD.map(lambda line: (line[aucid], 1))
+                      .reduceByKey(add)
+                      .sortBy(lambda line: line[1], ascending=False)
                       .take(5)
         )
 
-        print(top5Items.collect())
+        with open("/home/nadserver/spark-home/labs/05/Q6.txt", "w") as Q6:
+            print(top5Items, file=Q6)
 
         sc.stop()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print("Wrong inputs")
         sys.exit(-1)
     Answer().run(sys.argv[1], sys.argv[2])
