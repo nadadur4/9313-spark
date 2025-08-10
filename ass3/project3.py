cadd7da4b7df72bc13d3a48e48c481d05a8ac512
NadServer <nadserver@nadserver.local>
2025-08-11 00:12:26 +1000

    Started (conf has no *local* master)

--- ass3/project3.py
+++ ass3/project3.py
@@ -3,12 +3,28 @@ from pyspark import SparkConf, SparkContext
 from pyspark.sql.session import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+from operator import add
+
+def lineParse(line, prefix=''):
+    recId, coOrd, terms = line.strip().split('#')
+    numId = int(recId[1:])
+    x, y = coOrd[1:-1].split(',')
+    x, y = float(x), float(y)
+    tokens = set(terms.split())
+
+    parsedLine = (prefix, numId, recId, x, y, tokens)
+    return parsedLine
+
 
 class project3:
     def run(self, inputpathA, inputpathB, outputpath, d, s):
-        # You can use either RDD or DataFrame APIs
-
+        conf = SparkConf().setAppName("Project3")
+        sc = SparkContext(conf=conf)
         
+        rddA = sc.textFile(inputpathA).map(lambda line: lineParse(line, 'A'))
+        rddB = sc.textFile(inputpathB).map(lambda line: lineParse(line, 'B'))
+
+
 if __name__ == '__main__':
     if len(sys.argv) != 6:
         print("Wrong arguments")
